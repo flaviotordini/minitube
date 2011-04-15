c47f5c41f59c187f03ff6ea40798b440e0364a7a
Flavio <flavio@odisseo.local>
2011-04-15 16:54:47 +0200

    Reduce minimum video buffer now that we a retry mechanism

--- src/downloaditem.cpp
+++ src/downloaditem.cpp
@@ -186,7 +186,7 @@ void DownloadItem::downloadProgress(qint64 bytesReceived, qint64 bytesTotal) {
 
     if (m_status != Downloading) {
 
-        int neededBytes = (int) (bytesTotal * .01);
+        int neededBytes = (int) (bytesTotal * .001);
         // qDebug() << bytesReceived << bytesTotal << neededBytes << m_downloadTime.elapsed();
         int bufferSize = initialBufferSize();
         if (bytesReceived > bufferSize
