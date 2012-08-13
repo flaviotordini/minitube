c890f9eefc26abe90d1d4b2d170c3d9278719d9d
Flavio <flavio@odisseo.local>
2012-08-13 17:00:12 +0200

    Small build fix

--- src/searchparams.cpp
+++ src/searchparams.cpp
@@ -10,5 +10,5 @@ SearchParams::SearchParams() {
 
 void SearchParams::setParam(QString name, QVariant value) {
     bool success = setProperty(name.toUtf8(), value);
-    if (!success) qWarning() << "Failed to set property" << name << value;
+    if (!success) qWarning() << "Failed to set property" << name << value.toString();
 }
