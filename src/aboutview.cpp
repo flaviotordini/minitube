7b0ad8188a1e65e861e916a727625d33803e323a
Flavio Tordini <flavio.tordini@gmail.com>
2015-04-07 21:30:50 +0200

    It's 2015

--- src/aboutview.cpp
+++ src/aboutview.cpp
@@ -89,7 +89,7 @@ AboutView::AboutView(QWidget *parent) : QWidget(parent) {
             "<p>" + tr("Released under the <a href='%1'>GNU General Public License</a>")
             .arg("http://www.gnu.org/licenses/gpl.html") + "</p>"
         #endif
-            "<p>&copy; 2009-2014 " + Constants::ORG_NAME + "</p>"
+            "<p>&copy; 2009-2015 " + Constants::ORG_NAME + "</p>"
             "</body></html>";
     QLabel *infoLabel = new QLabel(info, this);
     infoLabel->setOpenExternalLinks(true);
