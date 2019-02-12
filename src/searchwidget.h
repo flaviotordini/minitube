60d6cad0e334aa6f689fe9d0e8e280f8804765b3
Flavio Tordini <flavio.tordini@gmail.com>
2019-02-12 17:45:40 +0100

    Update searchwidget.h

--- src/searchwidget.h
+++ src/searchwidget.h
@@ -9,7 +9,6 @@ class Suggestion;
 class AutoComplete;
 
 class SearchWidget {
-
 public:
     virtual QMenu *menu() const = 0;
     virtual void setMenu(QMenu *menu) = 0;
@@ -17,7 +16,7 @@ public:
     virtual void preventSuggest() = 0;
     virtual void selectAll() = 0;
     virtual void setSuggester(Suggester *suggester) = 0;
-    virtual void setInactiveText(const QString &text) = 0;
+    virtual void setPlaceholderText(const QString &text) = 0;
     virtual void setText(const QString &text) = 0;
     virtual AutoComplete *getAutoComplete() = 0;
     virtual void emitTextChanged(const QString &text) = 0;
@@ -31,7 +30,6 @@ signals:
     void textEdited(const QString &text);
     void search(const QString &text);
     void suggestionAccepted(Suggestion *suggestion);
-
 };
 
 #endif // SEARCHWIDGET
