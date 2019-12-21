b717e5c88617a2cfb814a9050982f37245bdefab
Flavio Tordini <flavio.tordini@gmail.com>
2019-12-21 01:10:47 +0100

    user agent

--- src/httputils.cpp
+++ src/httputils.cpp
@@ -56,6 +56,8 @@ const QByteArray &HttpUtils::userAgent() {
 }
 
 const QByteArray &HttpUtils::stealthUserAgent() {
-    static const QByteArray ua = "curl/7.37.0";
+    static const QByteArray ua =
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like "
+            "Gecko) Chrome/79.0.3945.79 Safari/537.36";
     return ua;
 }
