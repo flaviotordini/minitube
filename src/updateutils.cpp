753a70cc155174494edd8f9b196b74ebf1e1a821
Flavio Tordini <flavio.tordini@gmail.com>
2020-07-09 10:32:38 +0200

    Cleanup

--- src/updateutils.cpp
+++ src/updateutils.cpp
@@ -26,36 +26,21 @@ void init() {
 #else
     auto updater = new updater::DefaultUpdater();
 
-    // updater->setAppName(Constants::NAME);
-    // updater->setIcon(IconUtils::pixmap(":/images/app.png", qApp->devicePixelRatio()));
-    // updater->setLocalVersion(Constants::VERSION);
-
-    QUrl url(QLatin1String(Constants::WEBSITE) + "-ws/release.xml");
-    QUrlQuery q;
-    q.addQueryItem("v", Constants::VERSION);
-#ifdef APP_MAC
-    q.addQueryItem("os", "mac");
-#endif
-#ifdef APP_WIN
-    q.addQueryItem("os", "win");
-#endif
-#ifdef APP_MAC_STORE
-    q.addQueryItem("store", "mac");
-#endif
-    url.setQuery(q);
-    updater->setManifestUrl(url);
+    QUrl manifestUrl(QLatin1String(Constants::WEBSITE) + "-ws/release.xml");
+    updater->setManifestUrl(manifestUrl);
     updater->setParser(new updater::SimpleXmlParser());
 
-    QString ext = Constants::UNIX_NAME;
+    QString ext;
 #ifdef APP_MAC
     ext = ".dmg";
-#elif APP_WIN
+#elif defined APP_WIN
     ext = ".exe";
 #else
     ext = ".deb";
 #endif
-    updater->setDownloadUrl(QUrl("https://" + QLatin1String(Constants::ORG_DOMAIN) + "/files/" +
-                                 Constants::UNIX_NAME + "/" + Constants::UNIX_NAME + ext));
+    QUrl downloadUrl("https://" + QLatin1String(Constants::ORG_DOMAIN) + "/files/" +
+                     Constants::UNIX_NAME + "/" + Constants::UNIX_NAME + ext);
+    updater->setDownloadUrl(downloadUrl);
 
     Updater::setInstance(updater);
 #endif
