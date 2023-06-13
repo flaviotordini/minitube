b619e932a655c5c3ea96e0ab63e038778478706a
Flavio Tordini <flavio.tordini@gmail.com>
2023-06-13 16:09:03 +0200

    restore tooltips

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -321,11 +321,6 @@ bool MainWindow::eventFilter(QObject *obj, QEvent *e) {
         fullscreenTimer->start();
     }
 
-    if (t == QEvent::ToolTip) {
-        // kill tooltips
-        return true;
-    }
-
     if (t == QEvent::Show && obj == toolbarMenu) {
 #ifdef APP_MAC
         int x = width() - toolbarMenu->sizeHint().width();
